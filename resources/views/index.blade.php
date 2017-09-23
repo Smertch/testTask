db26ea410b26e6e2798a9a42422f847966d3898f
smertch <pomazsergej@gmail.com>
2017-09-23 20:17:03 +0300

    form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,11 +7,15 @@
 
         {{csrf_field()}}
         <div class="form-group">
-            <label for="title">Json:</label>
-            <input class="form-control" type="text" required name="json" id="json">
+            <label for="title">Feedback:</label>
+            <input class="form-control" type="text" name="userName" id="userName" placeholder="User Name*" required autofocus>
+            <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address*" required
+                   autofocus>
+            <input type="url" id="inputHomepage" name="homepage" class="form-control" placeholder="Homepage">
+            <input type="text" id="inputMsg" name="msg" class="form-control" placeholder="Enter your message*" required
+                   autofocus>
+            <p>*Required fields</p>
         </div>
-
-
         <div class="form-group">
             <button class="btn btn-primary" type="submit">Send</button>
         </div>
--- resources/views/layouts/headerNavigation.blade.php
+++ resources/views/layouts/headerNavigation.blade.php
@@ -4,14 +4,12 @@
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
             </button>
         </div>
         <div id="navbar" class="collapse navbar-collapse" >
             <ul class="nav navbar-nav">
-                <li class="active"><a href="/">Add</a></li>
-                <li><a href="/show">Show</a></li>
+                <li><a href="/">Feedback</a></li>
+                <li><a href="/show">Show email</a></li>
             </ul>
         </div>
     </div>
